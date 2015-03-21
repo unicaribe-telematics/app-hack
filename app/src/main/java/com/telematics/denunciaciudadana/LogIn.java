e6837d09eab63d72491a165af9177fa04641f640
unicaribe-telematics <javi.soul94@gmail.com>
2015-03-21 04:14:11 -0500

    cuarto commit

---
+++ app/src/main/java/com/telematics/denunciaciudadana/LogIn.java
@@ -0,0 +1,52 @@
+package com.telematics.denunciaciudadana;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.view.View.on
+
+
+public class LogIn extends ActionBarActivity {
+
+    final Button btnBoton1 = (Button) findViewById(R.id.button);
+    btnBoton1.setOnClickListener(new View.OnClickListener(){
+       @Override
+               public void onClick(findViewById())
+                {
+                    lblMensaje.setText("")
+                }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_log_in, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
